77a534bcff6066ee0f88e51f35459976b67cf21d
Robert Pavičić <rpavicic@racunarstvo.hr>
2018-02-07 19:00:24 +0100

    Empty model

--- NPP/MainForm.Designer.cs
+++ NPP/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NPP
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1264, 761);
+            this.Name = "Form1";
+            this.Text = "Printing viewer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
